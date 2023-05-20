a623d3157fbb6a08f06210e41a0b0bb002ce60b1
HieuNguyenFPOLY <manhhieu.nguyen1805@gmail.com>
2023-05-20 10:01:27 +0700

    Initial commit

---
+++ app/src/main/java/com/hieunguyen/fpoly/lab3_hieunmph32165/Bai2.java
@@ -0,0 +1,14 @@
+package com.hieunguyen.fpoly.lab3_hieunmph32165;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai2);
+    }
+}
---
+++ app/src/main/java/com/hieunguyen/fpoly/lab3_hieunmph32165/Bai3.java
@@ -0,0 +1,14 @@
+package com.hieunguyen.fpoly.lab3_hieunmph32165;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bai3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai3);
+    }
+}
